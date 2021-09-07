7afc7712fc4345f5b1441e8afdcb98fc83ccae42
Ulugbek Mannopov <umannopov3@gmail.com>
2021-09-07 13:05:48 +0500

    prettier chnages

--- src/pages/Firms.vue
+++ src/pages/Firms.vue
@@ -1,51 +1,55 @@
 <template>
-    <div :class="[isNavOpened ? 'nav' : '']" class="box">
-        <add-firm v-if="!forUpdate" :editedData="editedData" :isEdit="isEdit" @upping=upp($event)></add-firm>
-        <table-firms @editing="editData($event)"></table-firms>
-    </div>
+  <div :class="[isNavOpened ? 'nav' : '']" class="box">
+    <add-firm
+      v-if="!forUpdate"
+      :editedData="editedData"
+      :isEdit="isEdit"
+      @upping="upp($event)"
+    ></add-firm>
+    <table-firms @editing="editData($event)"></table-firms>
+  </div>
 </template>
 
 <script>
-import AddFirm from '../components/firms/AddFirm.vue'
-import TableFirms from '../components/firms/TableFirms.vue'
+import AddFirm from '../components/firms/AddFirm.vue';
+import TableFirms from '../components/firms/TableFirms.vue';
 
 export default {
-   components: {
-       "add-firm": AddFirm,
-       "table-firms": TableFirms
-       },
-    data(){
-        return{
-            editedData: null,
-            isEdit: false
-        }
+  components: {
+    'add-firm': AddFirm,
+    'table-firms': TableFirms
+  },
+  data() {
+    return {
+      editedData: null,
+      isEdit: false
+    };
+  },
+  computed: {
+    forUpdate() {
+      return this.$store.getters['firm/updated'];
     },
-    computed:{
-        forUpdate(){
-            return this.$store.getters['firm/updated']
-        },
-        isNavOpened(){
-            return this.$store.getters.isNav
-        }
-
+    isNavOpened() {
+      return this.$store.getters.isNav;
+    }
+  },
+  methods: {
+    editData(data) {
+      this.editedData = data;
+      this.isEdit = true;
     },
-    methods:{
-        editData(data){
-            this.editedData = data
-            this.isEdit = true
-        },
-        upp(val){
-            this.isEdit = val
-        }
-    },
-}
+    upp(val) {
+      this.isEdit = val;
+    }
+  }
+};
 </script>
 
 <style scoped>
-.box{
-    margin-left: 270px;
+.box {
+  margin-left: 270px;
 }
-.box.nav{
-    margin-left: 110px;
+.box.nav {
+  margin-left: 110px;
 }
 </style>

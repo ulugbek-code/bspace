fe026dd69d6dfc32ab6180bf8bd767ac6d0d0e7c
Ulugbek Mannopov <umannopov3@gmail.com>
2021-09-09 20:52:14 +0500

    getAll method for balanceUpload page

--- src/components/accounts/TableAccounts.vue
+++ src/components/accounts/TableAccounts.vue
@@ -1,254 +1,297 @@
 <template>
-    <div class="container">
-        <table>
-            <tr>
-                <th v-for="title in titles" :key="title">
-                    <h3>{{title}}</h3>
-                </th>
-            </tr>
-            <div v-if="getLoader" id="loader"><img src="../../assets/loader.gif" alt=""></div>
-            <div v-else-if="!getLoader && getError">{{getError}}</div>
-            <div v-else-if="!getLoader && (!getAccountsList || getAccountsList.length === 0)">
-                No stored firms yet or <br> Authorization problem
-            </div>
-            <tr v-for="content in filteredContents" :key="content">
-                <td>
-                    <span id="code" :style="{backgroundColor: `hsla(${~~(360 * Math.random())},70%,70%,0.8)`}">
-                        {{content.code}}
-                    </span>
-                </td>
-                <td>{{content.name}}</td>
-                <td :class="content.isActive ? 'blue-bg' : 'red-bg'">{{content.isActive ? 'Active' : 'Passive'}}</td>
-                <td id="category" >
-                    <span v-for="(c,i) in content.categories" :key="i">
-                        {{c.name}}{{++i !== content.categories.length ? ',' : ''}} <br>
-                    </span>
-                </td>
-                <td v-for="c in content.ifrsCategories" :key="c">{{c.code}}</td>
-                <td v-for="c in content.ifrsCategories" :key="c">{{c.name}}</td>
-                <td>
-                    <template v-for="(types,i) in content.ifrsCategories" :key="i">
-                        <template v-for="t in types.types" :key="t">{{t}}{{++i !== types.types.length ? ', ' : ' '}}</template>
-                    </template>
-                </td>
-                <div id="flex-div">
-                    <div id="edit-trash">
-                        <fa @click="editItem(content.id)" class="icons icon-edit" :icon="['fas', 'edit']" />
-                        <fa @click="deleteItem(content.id)" class="icons icon-trash" :icon="['fas', 'trash']" />
-                    </div>
-                </div>
-            </tr>
-        </table>
-    </div>
+  <div class="container">
+    <table>
+      <tr>
+        <th v-for="title in titles" :key="title">
+          <h3>{{ title }}</h3>
+        </th>
+      </tr>
+      <div v-if="getLoader" id="loader">
+        <img src="../../assets/loader.gif" alt="" />
+      </div>
+      <div v-else-if="!getLoader && getError">{{ getError }}</div>
+      <div
+        v-else-if="
+          !getLoader && (!getAccountsList || getAccountsList.length === 0)
+        "
+      >
+        No stored firms yet or <br />
+        Authorization problem
+      </div>
+      <tr v-for="content in filteredContents" :key="content">
+        <td>
+          <span
+            id="code"
+            :style="{
+              backgroundColor: `hsla(${~~(360 * Math.random())},70%,70%,0.8)`
+            }"
+          >
+            {{ content.code }}
+          </span>
+        </td>
+        <td>{{ content.name }}</td>
+        <td :class="content.isActive ? 'blue-bg' : 'red-bg'">
+          {{ content.isActive ? 'Active' : 'Passive' }}
+        </td>
+        <td id="category">
+          <span v-for="(c, i) in content.categories" :key="i">
+            {{ c.name }}{{ ++i !== content.categories.length ? ',' : '' }}
+            <br />
+          </span>
+        </td>
+        <td v-for="c in content.ifrsCategories" :key="c">{{ c.code }}</td>
+        <td v-for="c in content.ifrsCategories" :key="c">{{ c.name }}</td>
+        <td>
+          <template v-for="(types, i) in content.ifrsCategories" :key="i">
+            <template v-for="t in types.types" :key="t"
+              >{{ t }}{{ ++i !== types.types.length ? ', ' : ' ' }}</template
+            >
+          </template>
+        </td>
+        <div id="flex-div">
+          <div id="edit-trash">
+            <fa
+              @click="editItem(content.id)"
+              class="icons icon-edit"
+              :icon="['fas', 'edit']"
+            />
+            <fa
+              @click="deleteItem(content.id)"
+              class="icons icon-trash"
+              :icon="['fas', 'trash']"
+            />
+          </div>
+        </div>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    props:['search'],
-    data(){
-        return{
-            titles: ['CODE', 'NAME', 'TYPE', 'ACC CATEGORY', 'ACC N', 'IFRS KMPG', 'TYPES'],
-            categoriesName: [],
-            ifrsCategories: [],
-            ifrsTypes: [],
-            parentAccounts: [],
-            colors: ['red','blue','green']
-        }
+  props: ['search'],
+  data() {
+    return {
+      titles: [
+        'CODE',
+        'NAME',
+        'TYPE',
+        'ACC CATEGORY',
+        'ACC N',
+        'IFRS KMPG',
+        'TYPES'
+      ],
+      categoriesName: [],
+      ifrsCategories: [],
+      ifrsTypes: [],
+      parentAccounts: [],
+      colors: ['red', 'blue', 'green']
+    };
+  },
+  computed: {
+    filteredContents() {
+      return this.getAccountsList.filter(content => {
+        return (
+          content.name.match(this.search) ||
+          content.code.match(this.search) ||
+          content.ifrsCategories[0].code.match(this.search) ||
+          content.ifrsCategories[0].name.match(this.search)
+        );
+      });
     },
-    computed:{
-        filteredContents(){
-            return this.getAccountsList.filter((content)=>{
-                return content.name.match(this.search) || content.code.match(this.search) 
-                || content.ifrsCategories[0].code.match(this.search) || content.ifrsCategories[0].name.match(this.search)
-            })
-        },
-        getAccountsList(){
-            return this.$store.getters['account/getAccountList']
-        },
-        getParentAccountsList(){
-            return this.$store.getters['account/getParentAccountsList']
-        },
-        getCategoriesList(){
-            return this.$store.getters['account/getCategories']
-        },
-        getIfrsCategoriesList(){
-            return this.$store.getters['account/getIfrs']
-        },
-        getifrsTypes(){
-            return this.$store.getters['account/getifrsTypes']
-        },
-        getLoader(){
-            return this.$store.getters['account/getLoader']
-        },
-        getError(){
-            return this.$store.getters['account/getError']
-        }
+    getAccountsList() {
+      return this.$store.getters['account/getAccountList'];
     },
-    methods:{
-        deleteItem(id){
-            if(confirm("do you want to delete?")){
-                axios.delete("https://bspacedev.azurewebsites.net/api/Accounts/Delete/"+id,
-                {
-                    headers: {
-                            "Authorization" : `Bearer ${localStorage.getItem("mytoken")}`
-                        }
-                }
-                ).then(()=>{
-                    this.$store.dispatch("account/getAccounts",{
-                    isSubAcc: true,
-                    firmId: localStorage.getItem('firmId')}
-                    )
-                })
-                .catch(error => {
-                console.log(error.message);
-                })
-            }
-        },
-        async editItem(id){
-            await axios.get("https://bspacedev.azurewebsites.net/api/Accounts/GetById/"+id,
+    getParentAccountsList() {
+      return this.$store.getters['account/getParentAccountsList'];
+    },
+    getCategoriesList() {
+      return this.$store.getters['account/getCategories'];
+    },
+    getIfrsCategoriesList() {
+      return this.$store.getters['account/getIfrs'];
+    },
+    getifrsTypes() {
+      return this.$store.getters['account/getifrsTypes'];
+    },
+    getLoader() {
+      return this.$store.getters['account/getLoader'];
+    },
+    getError() {
+      return this.$store.getters['account/getError'];
+    }
+  },
+  methods: {
+    deleteItem(id) {
+      if (confirm('do you want to delete?')) {
+        axios
+          .delete(
+            'https://bspacedev.azurewebsites.net/api/Accounts/Delete/' + id,
             {
-                    headers: {
-                        "Authorization" : `Bearer ${localStorage.getItem("mytoken")}`
-                    }
-                }).then((res)=>{
-                    console.log(res)
-                    this.$emit("editAccounts",res.data.data)
-            })
-        }
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('mytoken')}`
+              }
+            }
+          )
+          .then(() => {
+            this.$store.dispatch('account/getAccounts', {
+              isSubAcc: true,
+              firmId: localStorage.getItem('firmId')
+            });
+          })
+          .catch(error => {
+            console.log(error.message);
+          });
+      }
     },
-    async created(){
-        await this.$store.dispatch("account/getAccounts",{
-            isSubAcc: false,
-            firmId: localStorage.getItem('firmId')})
-        await this.$store.dispatch("account/getAccounts",{
-            isSubAcc: true,
-            firmId: localStorage.getItem('firmId')})
-        
-        await this.$store.dispatch("account/getCategories")
-        await this.$store.dispatch("account/getIfrs")
-        await this.$store.dispatch("account/getifrsTypes")
-
-        this.categoriesName = this.getCategoriesList.map((category) => {
-        return {
-            name: category.name,
-            id: category.id}
-            })  
-        this.ifrsCategories = this.getIfrsCategoriesList.map((i)=>{
-            return {
-                    id: i.id,
-                    name: i.name,
-                    code: i.code}
+    async editItem(id) {
+      await axios
+        .get('https://bspacedev.azurewebsites.net/api/Accounts/GetById/' + id, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('mytoken')}`
+          }
         })
+        .then(res => {
+          console.log(res);
+          this.$emit('editAccounts', res.data.data);
+        });
+    }
+  },
+  async created() {
+    await this.$store.dispatch('account/getAccounts', {
+      isSubAcc: false,
+      firmId: localStorage.getItem('firmId')
+    });
+    await this.$store.dispatch('account/getAccounts', {
+      isSubAcc: true,
+      firmId: localStorage.getItem('firmId')
+    });
 
-        this.$emit('ifrs',this.ifrsCategories)
-        this.$emit('category',this.categoriesName)    
-        this.$emit('ifrsTypes',this.getifrsTypes)  
-        this.$emit('parentAccounts',this.getParentAccountsList)
-        
-    },
-}
+    await this.$store.dispatch('account/getCategories');
+    await this.$store.dispatch('account/getIfrs');
+    await this.$store.dispatch('account/getifrsTypes');
+
+    this.categoriesName = this.getCategoriesList.map(category => {
+      return {
+        name: category.name,
+        id: category.id
+      };
+    });
+    this.ifrsCategories = this.getIfrsCategoriesList.map(i => {
+      return {
+        id: i.id,
+        name: i.name,
+        code: i.code
+      };
+    });
+
+    this.$emit('ifrs', this.ifrsCategories);
+    this.$emit('category', this.categoriesName);
+    this.$emit('ifrsTypes', this.getifrsTypes);
+    this.$emit('parentAccounts', this.getParentAccountsList);
+  }
+};
 </script>
 
 <style scoped>
-.container{
-    overflow-y:scroll;
-    overflow-x: hidden;
-    height: 88vh;
-    /* display:block; */
-    width: 98%;
-    justify-content: space-between;
-    margin: 20px auto 0;
-    position: relative;
+.container {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 88vh;
+  /* display:block; */
+  width: 98%;
+  justify-content: space-between;
+  margin: 20px auto 0;
+  position: relative;
 }
-table{
-    border-collapse: collapse;
-    width: 100%;
-    
+table {
+  border-collapse: collapse;
+  width: 100%;
 }
-table tr:nth-child(1){
-    background: #fff;
-    position: sticky;
-    top: 0%;
-    z-index: 1;
+table tr:nth-child(1) {
+  background: #fff;
+  position: sticky;
+  top: 0%;
+  z-index: 1;
 }
-table th{
-    padding-left: 10px;
+table th {
+  padding-left: 10px;
 }
-table th::after{
-    content: '';
-    height: 0.5px;
-    width: 100%;
-    position: absolute;
-    bottom: 10%;
-    left:0;
-    background: rgba(170, 170, 170, 1);
+table th::after {
+  content: '';
+  height: 0.5px;
+  width: 100%;
+  position: absolute;
+  bottom: 10%;
+  left: 0;
+  background: rgba(170, 170, 170, 1);
 }
-th h3{
-    font-size: 16px;
-    color: rgba(170, 170, 170, 1);
-} 
-tr:not(:nth-child(1)){
-    border-bottom: 1px solid rgba(170, 170, 170, 0.2);
+th h3 {
+  font-size: 16px;
+  color: rgba(170, 170, 170, 1);
 }
-tr, td{
-    position: relative;
-    text-align: left;
-    height: 68px;
+tr:not(:nth-child(1)) {
+  border-bottom: 1px solid rgba(170, 170, 170, 0.2);
+}
+tr,
+td {
+  position: relative;
+  text-align: left;
+  height: 68px;
 }
 td:not(:nth-child(1)) {
-    padding: 10px 8px;
-    font-size: 14px;
+  padding: 10px 8px;
+  font-size: 14px;
 }
-td:nth-child(1){
-    padding-left: 5px;
+td:nth-child(1) {
+  padding-left: 5px;
 }
-tr:hover:not(:nth-child(1)){
-    border-radius: 15px;
-    box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
+tr:hover:not(:nth-child(1)) {
+  border-radius: 15px;
+  box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
 }
-tr:active:not(:nth-child(1)){
-        box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.4);
-
+tr:active:not(:nth-child(1)) {
+  box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.4);
 }
-tr:hover .icon-edit{
-    color: rgba(255, 255, 255, 1);
-    background: rgba(204, 24, 24, 0.75);
-    border-top-right-radius: 15px;
+tr:hover .icon-edit {
+  color: rgba(255, 255, 255, 1);
+  background: rgba(204, 24, 24, 0.75);
+  border-top-right-radius: 15px;
 }
-tr:hover .icon-trash{
-    color: rgba(255, 255, 255, 1);
-    background: rgba(67, 97, 238, 0.75);
-    border-bottom-right-radius: 15px;
+tr:hover .icon-trash {
+  color: rgba(255, 255, 255, 1);
+  background: rgba(67, 97, 238, 0.75);
+  border-bottom-right-radius: 15px;
 }
-#flex-div #edit-trash{
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    position: absolute;
-    right: 1%;
-    bottom: 0%;
+#flex-div #edit-trash {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  position: absolute;
+  right: 1%;
+  bottom: 0%;
 }
-.icons{
-    color: rgba(170, 170, 170, 1);
-    font-size: 34px;
-    width: 130%;
-    padding: 8px;
+.icons {
+  color: rgba(170, 170, 170, 1);
+  font-size: 34px;
+  width: 130%;
+  padding: 8px;
 }
-.red-bg{
-    color: rgba(181, 69, 60, 1);
+.red-bg {
+  color: rgba(181, 69, 60, 1);
 }
-.blue-bg{
-    color: rgba(67, 97, 238, 1);
+.blue-bg {
+  color: rgba(67, 97, 238, 1);
 }
-#code{
-    padding: 4px 8px;
-    border-radius: 15px;
-    font-size: 14px;
+#code {
+  padding: 4px 8px;
+  border-radius: 15px;
+  font-size: 14px;
 }
-#loader{
+#loader {
   position: relative;
   left: 0;
   top: 0;
@@ -257,10 +300,10 @@ tr:hover .icon-trash{
   background: #f1f2f3;
   z-index: 100;
 }
-#loader img{
+#loader img {
   position: relative;
   top: 40%;
   left: 40%;
-  transform: translate(-40%,-40%);
+  transform: translate(-40%, -40%);
 }
 </style>
--- src/components/balanceUpload/AddedBalance.vue
+++ src/components/balanceUpload/AddedBalance.vue
@@ -1,215 +1,203 @@
 <template>
-    <div id="added-balance">
-        <h3>Balance List</h3>
-        <div class="table-container">
-            <table>
-                <tr>
-                    <th v-for="title in titles" :key="title">
-                        <h4>{{title}} <fa class="arrows" :icon="['fas', 'arrows-alt-v']" /></h4>
-                    </th>
-                </tr>
-                <tr @click="testTr" v-for="balance in balances" :key="balance">
-                    <td>
-                        {{balance.year}}
-                    </td>
-                    <td>
-                        {{balance.period}}
-                    </td>
-                    <td>
-                        {{substringedDesc(balance.desc)}}
-                        <small class="tooltiptext">{{balance.desc}}</small>
-                    </td>
-                    <td :class="[balance.status === 'Accepted' ? 'greeny' : 'redish']">
-                        <p>{{balance.status}}</p>
-                    </td>
-                    <td @click.stop="testTrash" id="trash">
-                        <fa :icon="['fas', 'trash']" />
-                    </td>
-                </tr>
-            </table>
-        </div>
+  <div id="added-balance">
+    <h3>Balance List</h3>
+    <div class="table-container">
+      <table v-if="balances.length > 0">
+        <tr>
+          <th v-for="title in titles" :key="title">
+            <h4>
+              {{ title }} <fa class="arrows" :icon="['fas', 'arrows-alt-v']" />
+            </h4>
+          </th>
+        </tr>
+        <tr @click="testTr" v-for="balance in balances" :key="balance">
+          <td>
+            {{ balance.year }}
+          </td>
+          <td>
+            {{ balance.period }}
+          </td>
+          <td>
+            {{ substringedDesc(balance.desc) }}
+            <small class="tooltiptext">{{ balance.desc }}</small>
+          </td>
+          <td :class="[balance.status === 'Accepted' ? 'greeny' : 'redish']">
+            <p>{{ balance.status }}</p>
+          </td>
+          <td @click.stop="testTrash" id="trash">
+            <fa :icon="['fas', 'trash']" />
+          </td>
+        </tr>
+      </table>
+
+      <div v-if="errors.length > 0">There is no balances</div>
     </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    data(){
-        return{
-            titles:['Year','Period','Description','Status'],
-            balances:[
-                {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad',
-                    status: 'In Progress'
-                },
-                {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad',
-                    status: 'Accepted'
-                },
-                {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad adadad dadada  addadd',
-                    status: 'In Progress'
-                },
-                {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsadasdd sdad',
-                    status: 'Accepted'
-                },
-                {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad',
-                    status: 'In Progress'
-                },
-                 {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad',
-                    status: 'In Progress'
-                },
-                 {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad',
-                    status: 'In Progress'
-                },
-                 {
-                    year: 1199,
-                    period: 'March',
-                    desc: 'lalda adakj da sda adsd dsad',
-                    status: 'In Progress'
-                }
-            ]
-        }
+  data() {
+    return {
+      errors: [],
+      titles: ['Year', 'Period', 'Description', 'Status'],
+      balances: [
+        // {
+        //   year: 1199,
+        //   period: 'March',
+        //   desc: 'lalda adakj da sda adsd dsad',
+        //   status: 'In Progress'
+        // }
+      ]
+    };
+  },
+  methods: {
+    testTr() {
+      console.log('hello row');
     },
-    methods:{
-        testTr(){
-            console.log('hello row')
-        },
-        testTrash(){
-            console.log('hello trash')
-        },
-        substringedDesc(val){
-            if(val.length > 45){
-                return (val.substring(0, 45) + '...')
-            }else{
-                return val
+    testTrash() {
+      console.log('hello trash');
+    },
+    substringedDesc(val) {
+      if (val.length > 45) {
+        return val.substring(0, 45) + '...';
+      } else {
+        return val;
+      }
+    },
+    getAllBalances(firmId) {
+      axios
+        .get(
+          'https://bspacedev.azurewebsites.net/api/BalanceFiles/GetAll/' +
+            firmId,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('mytoken')}`
             }
-        }
+          }
+        )
+        .then(res => {
+          if (res.data.isValid) {
+            this.balances = res.data.data;
+          } else {
+            this.errors = res.data.errors;
+          }
+        })
+        .catch(err => console.log(err));
     }
-}
+  },
+  created() {
+    this.getAllBalances(localStorage.getItem('firmId'));
+  }
+};
 </script>
 
 <style scoped>
-#added-balance{
-    width: 60%;
-    background: rgba(221, 221, 221, 0.4);
-    padding: 20px 0 30px 20px;
-    /* margin-bottom: 20px; */
-    
+#added-balance {
+  width: 60%;
+  background: rgba(221, 221, 221, 0.4);
+  padding: 20px 0 30px 20px;
+  /* margin-bottom: 20px; */
 }
-#added-balance .table-container{
-    width: 98%;
-    height: 100%;
-    overflow-y: scroll;
-    overflow-x: hidden;
+#added-balance .table-container {
+  width: 98%;
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
 }
-table{
-    width: 100%;
-    border-collapse: collapse;
-    text-align: left;
+table {
+  width: 100%;
+  border-collapse: collapse;
+  text-align: left;
+  background: blue;
 }
-table tr:nth-child(1){
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    background: #f1f1f1;
+table tr:nth-child(1) {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background: #f1f1f1;
 }
-table tr:nth-child(1)::after{
-    content: '';
-    height: 0.5px;
-    width: 100%;
-    position: absolute;
-    bottom: 2%;
-    left:0;
-    z-index: 10;
-    background: rgba(170, 170, 170, 1);
+table tr:nth-child(1)::after {
+  content: '';
+  height: 0.5px;
+  width: 100%;
+  position: absolute;
+  bottom: 2%;
+  left: 0;
+  z-index: 10;
+  background: rgba(170, 170, 170, 1);
 }
-th h4{
-    font-size: 16px;
-    padding-left: 10px;
-    color: rgb(133, 127, 127);
+th h4 {
+  font-size: 16px;
+  padding-left: 10px;
+  color: rgb(133, 127, 127);
 }
-.arrows{
-    font-size: 13px;
-    padding-bottom: 2px;
+.arrows {
+  font-size: 13px;
+  padding-bottom: 2px;
 }
-th:nth-child(3) .arrows{
-    display: none;
+th:nth-child(3) .arrows {
+  display: none;
 }
-tr:hover:not(:nth-child(1)){
-    border-radius: 15px;
-    box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.1);
-    background: #fff;
-    cursor: pointer;
+tr:hover:not(:nth-child(1)) {
+  border-radius: 15px;
+  box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.1);
+  background: #fff;
+  cursor: pointer;
 }
-td{
-    position: relative;
-    padding: 8px 12px;
-    font-size: 13px;
+td {
+  position: relative;
+  padding: 8px 12px;
+  font-size: 13px;
 }
-td:nth-child(1){
-    border-top-left-radius:15px;
-    border-bottom-left-radius:15px
+td:nth-child(1) {
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
 }
-td:last-child{
-    border-top-right-radius:15px;
-    border-bottom-right-radius:15px
+td:last-child {
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
 }
-.greeny p{
-    text-align: center;
-    padding: 3px;
-    border-radius: 25px;
-    background: rgba(155, 222, 157, 0.3);
-    color: rgba(76, 175, 80, 1);
+.greeny p {
+  text-align: center;
+  padding: 3px;
+  border-radius: 25px;
+  background: rgba(155, 222, 157, 0.3);
+  color: rgba(76, 175, 80, 1);
 }
-.redish p{
-    text-align: center;
-    padding: 3px;
-    border-radius: 25px;
-    background: rgba(255, 178, 125, 0.3);
-    color: rgba(237, 125, 43, 1);
+.redish p {
+  text-align: center;
+  padding: 3px;
+  border-radius: 25px;
+  background: rgba(255, 178, 125, 0.3);
+  color: rgba(237, 125, 43, 1);
 }
 .tooltiptext {
-    visibility: hidden;
-    background-color: rgb(39, 43, 61);
-    color: #fff;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    text-align: center;
-    border-radius: 6px;
-    padding: 5px; 
-    width: fit-content;
-    position: absolute;
-    top: 70%;
-    left: 4%;
-    z-index: 1;
+  visibility: hidden;
+  background-color: rgb(39, 43, 61);
+  color: #fff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  width: fit-content;
+  position: absolute;
+  top: 70%;
+  left: 4%;
+  z-index: 1;
 }
 td:nth-child(3):hover .tooltiptext {
   visibility: visible;
 }
-#trash{
-    font-size: 16px;
-    color: rgba(170, 170, 170, 1);
-    padding: 4px;
-    cursor:pointer;
+#trash {
+  font-size: 16px;
+  color: rgba(170, 170, 170, 1);
+  padding: 4px;
+  cursor: pointer;
 }
-#trash:hover{
-    color: rgba(204, 24, 24, 0.75);
+#trash:hover {
+  color: rgba(204, 24, 24, 0.75);
 }
 </style>
